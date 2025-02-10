70de4eb0b2d0667af82b0ad084eec4576d8b7e22
dk <d.kozhemyako@icloud.com>
2025-02-10 21:33:59 +0200

    Initial commit for C# Rectangle project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_1;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть координати двох протилежних вершин прямокутника:");
+        Console.Write("x1: ");
+        double x1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("y1: ");
+        double y1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("x2: ");
+        double x2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("y2: ");
+        double y2 = Convert.ToDouble(Console.ReadLine());
+
+        Rectangle rect = new Rectangle(x1, y1, x2, y2);
+
+        Console.WriteLine("\nІнформація про прямокутник:");
+        Console.WriteLine(rect.GetInfo());
+
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab_1;
+public class Rectangle
+{ 
+    private double x1, y1, x2, y2;
+
+    public Rectangle(double x1, double y1, double x2, double y2)
+    {
+        this.x1 = Math.Min(x1, x2);
+        this.y1 = Math.Min(y1, y2);
+        this.x2 = Math.Max(x1, x2);
+        this.y2 = Math.Max(y1, y2);
+
+    }
+    private double Width() => x2 - x1;
+    private double Height() => y2 - y1;
+
+    public double Area() => Width() * Height();
+    public double Perimeter() => 2 * (Width() + Height());
+
+    public string GetInfo()
+    {
+        return $"Прямокутник: Вершини ({x1}, {y1}), ({x2}, {y2})\n" +
+              $"Ширина: {Width()}, Висота: {Height()}\n" +
+              $"Площа: {Area()}, Периметр: {Perimeter()}";
+    }
+}
