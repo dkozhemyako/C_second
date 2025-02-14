e42ac25337a623a6bb8b0fed83665b5e5aff0532
dk <d.kozhemyako@icloud.com>
2025-02-14 09:51:15 +0200

    version 2

--- Program.cs
+++ Program.cs
@@ -5,20 +5,31 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Введіть координати двох протилежних вершин прямокутника:");
+        Console.WriteLine("Введіть координати чотирьох вершин прямокутника:");
+
         Console.Write("x1: ");
         double x1 = Convert.ToDouble(Console.ReadLine());
         Console.Write("y1: ");
         double y1 = Convert.ToDouble(Console.ReadLine());
+
         Console.Write("x2: ");
         double x2 = Convert.ToDouble(Console.ReadLine());
         Console.Write("y2: ");
         double y2 = Convert.ToDouble(Console.ReadLine());
 
-        Rectangle rect = new Rectangle(x1, y1, x2, y2);
+        Console.Write("x3: ");
+        double x3 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("y3: ");
+        double y3 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("x4: ");
+        double x4 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("y4: ");
+        double y4 = Convert.ToDouble(Console.ReadLine());
+
+        Rectangle rect = new Rectangle(x1, y1, x2, y2, x3, y3, x4, y4);
 
         Console.WriteLine("\nІнформація про прямокутник:");
         Console.WriteLine(rect.GetInfo());
-
     }
 }
--- Rectangle.cs
+++ Rectangle.cs
@@ -2,26 +2,34 @@
 
 namespace Lab_1;
 public class Rectangle
-{ 
-    private double x1, y1, x2, y2;
+{
+    private double x1, y1, x2, y2, x3, y3, x4, y4;
 
-    public Rectangle(double x1, double y1, double x2, double y2)
+    public Rectangle(double x1, double y1, double x2, double y2,
+                     double x3, double y3, double x4, double y4)
     {
-        this.x1 = Math.Min(x1, x2);
-        this.y1 = Math.Min(y1, y2);
-        this.x2 = Math.Max(x1, x2);
-        this.y2 = Math.Max(y1, y2);
-
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.x3 = x3;
+        this.y3 = y3;
+        this.x4 = x4;
+        this.y4 = y4;
     }
-    private double Width() => x2 - x1;
-    private double Height() => y2 - y1;
+
+    private double Distance(double x1, double y1, double x2, double y2) =>
+        Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+    private double Width() => Distance(x1, y1, x2, y2);
+    private double Height() => Distance(x2, y2, x3, y3);
 
     public double Area() => Width() * Height();
     public double Perimeter() => 2 * (Width() + Height());
 
     public string GetInfo()
     {
-        return $"Прямокутник: Вершини ({x1}, {y1}), ({x2}, {y2})\n" +
+        return $"Прямокутник: Вершини ({x1}, {y1}), ({x2}, {y2}), ({x3}, {y3}), ({x4}, {y4})\n" +
               $"Ширина: {Width()}, Висота: {Height()}\n" +
               $"Площа: {Area()}, Периметр: {Perimeter()}";
     }
